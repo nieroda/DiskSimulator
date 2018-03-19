b1ba87a06c10a72cf2bc4491c7a973bda762ceb0
Nathan Kamm <eathotlead@gmail.com>
2018-03-19 14:42:36 -0700

    Update OutputCreator.js

--- EventNode/OutputCreator.js
+++ EventNode/OutputCreator.js
@@ -1,14 +1,10 @@
 
 
-let getRandomTrack = () => ( Math.floor( Math.random() * 99 ) );
-let getRandomSector = () => ( Math.floor( Math.random() * 29 ) );
+const getRandomTrack  = () => Math.floor(Math.random() * 99)
+const getRandomSector = () => Math.floor(Math.random() * 29)
 
-function getAll() {
-    for (let i = 20; i < 70000; i += Math.floor(Math.random() * 20)) {
-        console.log(`${i} ${getRandomTrack()} ${getRandomSector()}`);
-    }
-}
+(() => {
+    for (let i = 20; i < 70000; i += Math.floor(Math.random() * 20)) 
+        console.log(`${i} ${getRandomTrack()} ${getRandomSector()}`)
+})()
 
-
-
-getAll();
